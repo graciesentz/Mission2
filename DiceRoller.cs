9159ca7a2385e20082c1ef4ef3bf914ab34d3f8e
Gracie Sentz <gs447@byu.edu>
2024-01-17 15:15:02 -0700

    added comments

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -14,12 +14,14 @@ namespace Mission2
         {
             int[] results = new int[13];
 
+            // simulate each roll
             for (int i = 0; i < numberOfRolls; i++)
             {
-                int dice1 = random.Next(1, 7);
+                int dice1 = random.Next(1, 7); //6 sides on a die
                 int dice2 = random.Next(1, 7);
                 int sum = dice1 + dice2;
 
+                // add the sum to results
                 results[sum]++;
             }
 
--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ internal class Program
     {
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
 
+        // ask how many rolls the user wants
         System.Console.WriteLine("How many dice rolls would you like to simulate?");
 
+        // get the number of rolls from user input
         int numRolls = int.Parse(Console.ReadLine());
 
+        // create instance of the class
         DiceRoller diceRoller = new DiceRoller();
 
+        // store results in results array
         int[] results = diceRoller.SimulateRolls(numRolls);
 
         PrintHistogram(results, numRolls);
@@ -19,12 +23,14 @@ internal class Program
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 
+    //print the histogram
     static void PrintHistogram(int[] results, int totalRolls)
     {
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine($"Total number of rolls = {totalRolls}.\n");
 
+        // print the stars based on the results
         for (int i = 2; i <= 12; i++)
         {
             int percentage = results[i] * 100 / totalRolls;
